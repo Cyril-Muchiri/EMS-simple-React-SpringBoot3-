f33a804baf0a9cf959882c25dfca75760fe2d587
Cyril-Muchiri <cyrilmuchiri11@gmail.com>
2024-02-12 22:07:48 +0300

    added a footer

--- ems-backend/src/main/java/com/ems/emsbackend/rest/EmployeeController.java
+++ ems-backend/src/main/java/com/ems/emsbackend/rest/EmployeeController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,14 +14,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ems.emsbackend.entity.Employee;
 import com.ems.emsbackend.services.EmployeeServiceI;
 
+import lombok.AllArgsConstructor;
+
+@CrossOrigin("*")
 @RestController
-@RequestMapping("/api/employee")
+@RequestMapping("/api/employees")
+@AllArgsConstructor
 public class EmployeeController {
 
     @Autowired
