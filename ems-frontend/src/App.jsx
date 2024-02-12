93d59e6772639995119266f131cfee50faf59580
Cyril-Muchiri <cyrilmuchiri11@gmail.com>
2024-02-12 22:08:40 +0300

    added @cross Origin-cors annotation

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -3,11 +3,15 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import ListEmployee from "./components/ListEmployee";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
+    <Header></Header>
       <ListEmployee></ListEmployee>
+      <Footer></Footer>
     </>
   );
 }
