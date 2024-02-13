e1eef4a12d74e6fcdcd65318e24568ff2e940fab
Cyril-Muchiri <cyrilmuchiri11@gmail.com>
2024-02-13 11:51:48 +0300

    added a field for first name in employee component

--- ems-frontend/src/components/EmployeeComponent.jsx
+++ ems-frontend/src/components/EmployeeComponent.jsx
@@ -1,11 +1,34 @@
 import React, { useState } from "react";
 
 const EmployeeComponent = () => {
-    
+
   const [firstName, setFirstname] = useState("");
   const [lastName, setLastname] = useState("");
   const [email, setEmail] = useState("");
-  return <div></div>;
+
+  return( 
+  <div className="container"> 
+  <div className="row">
+    <div className="card">
+        <h2 className="text-center">add employee</h2>
+        <div className="card-body">
+            <form action="">
+                <div className="form-group mb-2">
+                    <label className="form-label">firstName :</label>
+                    <input type="text"
+                    placeholder="Enter employee first name"
+                    name="firstName"
+                    value={firstName} />
+
+                </div>
+            </form>
+        </div>
+    </div>
+
+  </div>
+
+  </div>
+  )
 };
 
 export default EmployeeComponent;
